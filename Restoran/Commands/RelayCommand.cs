19664f11ec9cad8655542189c7092fc84f849600
Humba_mv29 <Humba_mv29@STHQ012E-02>
2023-04-04 12:51:44 +0400

    Add project files.

---
+++ Restoran/Commands/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using Restoran.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restoran.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public void Execute(object? parameter)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        public bool CanExecute(object? parameter)=>true;
+
+
+    }
+}
